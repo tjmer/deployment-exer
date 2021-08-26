d39dd4be162a8bf0bbf5f72fe5063f0482ec0d48
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-26 16:37:09 -0600

    Cleaned up a bit

--- deployment/index.js
+++ deployment/index.js
@@ -4,3 +4,25 @@ const theMixerBtn = document.querySelector('#yumm3')
 const teethAndYummyBtn = document.querySelector('#yumm4')
 const lightAsAirBtn = document.querySelector('#yumm5')
 const cherryOnTopBtn = document.querySelector('#yumm6')
+const clearCart = document.querySelector('#clear')
+const cart = document.querySelector('#numbers')
+
+let count = 0
+
+yumBtn.addEventListener('click', cartIncrease)
+rosieBtn.addEventListener('click', cartIncrease)
+theMixerBtn.addEventListener('click', cartIncrease)
+teethAndYummyBtn.addEventListener('click', cartIncrease)
+lightAsAirBtn.addEventListener('click', cartIncrease)
+cherryOnTopBtn.addEventListener('click', cartIncrease)
+clearCart.addEventListener('click', emptyCart)
+
+function cartIncrease(){
+    count = count + 1
+    cart.textContent = count;
+}
+
+function emptyCart(){
+    count = 0
+    cart.textContent = count
+}
