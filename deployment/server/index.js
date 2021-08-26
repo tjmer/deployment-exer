fe807cadc2fc101de66d7f226ad791e2ed8701a3
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-26 14:03:53 -0600

    Added back stuff with camma

--- deployment/server/index.js
+++ deployment/server/index.js
@@ -7,6 +7,14 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
+app.get("/js", function(req, res){
+    res.sendFile(path.join(__dirname, "../index.js"))
+})
+
+app.get("/css", function(req, res) {
+    res.sendFile(path.join(__dirname, "../styles.css"))
+})
+
 const port = process.env.PORT || 4004
 
 app.listen(port, ()=>{
