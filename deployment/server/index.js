3a4fbdb70c9064f459be51570b4284a800e13669
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-26 13:32:31 -0600

    added css and js

--- deployment/server/index.js
+++ deployment/server/index.js
@@ -7,6 +7,14 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join("../styles.css"))
+})
+
+app.get("/", function(req, res){
+    res.sendFile(path.join("../index.js"))
+})
+
 const port = process.env.PORT || 4004
 
 app.listen(port, ()=>{
