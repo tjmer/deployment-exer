7ab290998e440fc439dfdba79b70169ea9c9e4f4
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-26 13:45:28 -0600

    removed a get

--- deployment/server/index.js
+++ deployment/server/index.js
@@ -7,10 +7,6 @@ app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "../index.html"))
 })
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join("../public/styles.css"))
-})
-
 app.get("/", function(req, res){
     res.sendFile(path.join("../public/index.js"))
 })
