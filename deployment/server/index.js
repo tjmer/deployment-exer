9caf79f53619f82767af98e86aed456393513815
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-26 13:59:26 -0600

    moved into one get

--- deployment/server/index.js
+++ deployment/server/index.js
@@ -5,15 +5,18 @@ const app = express()
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "../index.html"))
+    res.sendFile(path.join(__dirname"../index.js"))
+    res.sendFile(path.join(__dirname"../styles.css"))
+    
 })
 
-app.get("/js", function(req, res){
-    res.sendFile(path.join("../index.js"))
-})
+// app.get("/js", function(req, res){
+//     res.sendFile(path.join(__dirname"../index.js"))
+// })
 
-app.get("/css", function(req, res) {
-    res.sendFile(path.join("../public/styles.css"))
-})
+// app.get("/css", function(req, res) {
+//     res.sendFile(path.join(__dirname"../index.js"))
+// })
 
 const port = process.env.PORT || 4004
 
